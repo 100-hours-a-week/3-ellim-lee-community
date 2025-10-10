863a6a537933a5075fcaf35ec0af7f225245f8a9
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-10-11 05:32:29 +0900

    refactor: AuthorResponse imageUrl 변수명 변경 -> profileImageUrl

--- src/main/java/gguip1/community/domain/user/dto/AuthorResponse.java
+++ src/main/java/gguip1/community/domain/user/dto/AuthorResponse.java
@@ -6,11 +6,11 @@ import lombok.Data;
  * 작성자 정보 응답 DTO입니다. (comment, post 작성자 등)
  * Fields:
  *  nickname - 작성자의 닉네임
- *  imageUrl - 작성자의 프로필 이미지 URL
+ *  profileImageUrl - 작성자의 프로필 이미지 URL
  */
 @Data
 public class AuthorResponse {
 //    private Integer userId;
     private String nickname;
-    private String imageUrl;
+    private String profileImageUrl;
 }
