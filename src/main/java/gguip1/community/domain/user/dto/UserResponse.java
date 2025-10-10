1e59f76eacddb3c34e629eab4ea81b32ce695ffb
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-10-11 05:02:20 +0900

    feat: User Request, Response DTO 생성

---
+++ src/main/java/gguip1/community/domain/user/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package gguip1.community.domain.user.dto;
+
+import lombok.Data;
+
+/**
+ * 사용자 응답 DTO입니다. (내 정보 조회 등)
+ * Fields:
+ *  email - 사용자의 이메일
+ *  nickname - 사용자의 닉네임
+ *  imageUrl - 프로필 이미지 URL
+ */
+@Data
+public class UserResponse {
+//    private Integer id;
+    private String email;
+    private String nickname;
+    private String imageUrl;
+}
---
+++ src/main/java/gguip1/community/domain/user/dto/UserUpdateRequest.java
@@ -0,0 +1,15 @@
+package gguip1.community.domain.user.dto;
+
+import lombok.Data;
+
+/**
+ * 사용자 정보 수정 요청 DTO입니다.
+ * Fields:
+ *  nickname - 사용자의 닉네임
+ *  imageUrl - 프로필 이미지 URL
+ */
+@Data
+public class UserUpdateRequest {
+    private String nickname;
+    private String imageUrl;
+}
