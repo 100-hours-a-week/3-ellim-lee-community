1a4dada6672ba6b4935eb859101670ce23cbceed
gguip1 <51047947+gguip1@users.noreply.github.com>
2025-10-11 12:57:28 +0900

    feat: ApiResponse에 @AllArgsConstructor 생성자 추가

--- src/main/java/gguip1/community/global/response/ApiResponse.java
+++ src/main/java/gguip1/community/global/response/ApiResponse.java
@@ -1,8 +1,11 @@
 package gguip1.community.global.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class ApiResponse<T> {
     private String message;
     private T data;
