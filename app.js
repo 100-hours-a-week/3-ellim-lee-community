4cf3913b9e15612a9ebb58ea599e65c9425e63d9
gguip1 <gguip7554@naver.com>
2025-10-23 09:46:01 +0900

    feat: 임시 404 페이지 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Path = require('path');
 const app = express();
 const port = 3000;
 
+const allwedPaths = ['/posts', '/users'];
+
 app.use(express.static(Path.join(__dirname, 'public')));
 app.use('/assets', express.static(Path.join(__dirname, 'assets')));
 
@@ -11,6 +13,14 @@ app.use('/posts', postRouter);
 
 const userRouter = require('./public/pages/users/js/userRouter');
 app.use('/users', userRouter);
+
+app.use((req, res) => {
+    if (allwedPaths.some(path => req.path.startsWith(path))) {
+        res.status(404).sendFile(Path.join(__dirname, 'public', '/pages/error/404.html'));
+    } else {
+        res.status(404).send('404 Not Found');
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
